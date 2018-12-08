f9940de0828306db10aaa6dc5429558d28530d61
Pedro Ramos <ppedro.ramoss@gmail.com>
2018-12-08 17:11:19 -0200

    Create send function that doesnt send anything just yet

--- services/email-service/handler.js
+++ services/email-service/handler.js
@@ -1,14 +1,11 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.sendEmail = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Nothing was sent because this isn\'t ready yet :)',
       input: event,
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
